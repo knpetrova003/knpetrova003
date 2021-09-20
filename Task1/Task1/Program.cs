b5595f205f70f5c52bd9f8705d3cf62057707617
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-09-20 14:25:30 +0300

    Вывод стиха.

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Анна Ахматова Всё расхищено, предано, продано.");
+            {
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                {
+                    Console.WriteLine("О, как на склоне наших лет Нежней мы любим и суеверней…");
+                    Console.ReadKey();
+                    Console.WriteLine("Сияй, сияй, прощальный свет Любви последней, зари вечерней!");
+                    Console.ReadKey();
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("Анна Ахматова Всё расхищено, предано, продано.");
+                        Console.WriteLine("О, как на склоне наших лет Нежней мы любим и суеверней…");
+                        Console.WriteLine("Сияй, сияй, прощальный свет Любви последней, зари вечерней!");
+                    }
+                }
+            }
+
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+
+        }
+    }
+}
+
+
+
