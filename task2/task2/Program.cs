de09afe569527e2341b6ef39edd090c17cecd568
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-10-18 11:49:40 +0300

    hw2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Объем икосаэдра:{(5 * (3 + Math.Sqrt(5)) * a * a * a) / 12}");
+            Console.WriteLine($"Площадь треугольника:{(5 * a * a * Math.Sqrt(3))}");
+            Console.ReadKey();
+        }
+    }
+}
+
