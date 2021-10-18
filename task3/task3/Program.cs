b107ffac8dab03756eb6fbf0b8df6993f59ba8e4
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-10-18 13:20:23 +0300

    hw3

---
+++ task3/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Math.Abs(2 * Math.Sin(-3 * Math.Abs((x + 1) / 2)));
+            Console.WriteLine("y={0}", y);
+            Console.ReadLine();
+        }
+    }
+}
