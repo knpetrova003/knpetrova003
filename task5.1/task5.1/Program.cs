1e1ec58198b11b9f8e071f9db7752a6fa17949c7
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-10-27 23:44:30 +0300

    hw5.2

--- task5.1/task5.1/Program.cs
+++ task5.1/task5.1/Program.cs
@@ -6,44 +6,28 @@ namespace task5._1
     {
         static void Main(string[] args)
         {
-            string str = Console.ReadLine().ToUpper();
-            Console.WriteLine(str);
-            str = str.Replace("А", "A");
-            str = str.Replace("Б", "6");
-            str = str.Replace("В", "B");
-            str = str.Replace("Г", "r");
-            str = str.Replace("Д", "D");
-            str = str.Replace("Е", "E");
-            str = str.Replace("Ё", "E");
-            str = str.Replace("Ж", "}|{");
-            str = str.Replace("З", "3");
-            str = str.Replace("И", "u");
-            str = str.Replace("Й", "u*");
-            str = str.Replace("К", "K");
-            str = str.Replace("Л", "JI");
-            str = str.Replace("М", "M");
-            str = str.Replace("Н", "H");
-            str = str.Replace("О", "O");
-            str = str.Replace("П", "n");
-            str = str.Replace("Р", "P");
-            str = str.Replace("С", "C");
-            str = str.Replace("Т", "T");
-            str = str.Replace("У", "Y");
-            str = str.Replace("Ф", "cp");
-            str = str.Replace("Х", "X");
-            str = str.Replace("Ц", "L|");
-            str = str.Replace("Ч", "4");
-            str = str.Replace("Ш", "LLI");
-            str = str.Replace("Щ", "LLL");
-            str = str.Replace("Ь", "b");
-            str = str.Replace("Ы", "bI");
-            str = str.Replace("Ъ", "`b");
-            str = str.Replace("Э", "-)");
-            str = str.Replace("Ю", "IO");
-            str = str.Replace("Я", "9I");
+            //  Слово перевозка,  веревка и повозка
+            string str = "";
 
+            str = "перевозка";//п-0 е-1 р-2 е-3 в-4 о-5 з-6 к-7 а-8
             Console.WriteLine(str);
+
+            string result = str.Substring(4, 1)
+                + str.Substring(1, 4)
+                + str.Substring(7, 2);
+
+
+            Console.WriteLine(result);
+            string resalt = str.Substring(0,1)
+                +str.Substring(5, 1)
+                +str.Substring(4);
+
+            Console.WriteLine(resalt);
+            Console.ReadKey();
+
+
 
         }
     }
 }
+
