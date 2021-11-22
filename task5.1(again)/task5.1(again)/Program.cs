997355b3563b16e5561ab9decd66ee0b98728f61
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-11-22 13:40:19 +0300

    task5.1

---
+++ task5.1(again)/task5.1(again)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5._1_again_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
