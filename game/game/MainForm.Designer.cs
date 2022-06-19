86f8dbeb36bea8b4dc1fd13acf8b55c7ad4dec6b
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2022-06-19 20:36:49 +0500

    exam2

---
+++ game/game/MainForm.Designer.cs
@@ -0,0 +1,370 @@
+﻿
+namespace risovach
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuItemExit = new System.Windows.Forms.MenuStrip();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemBack = new System.Windows.Forms.MenuStrip();
+            this.назадToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemStart = new System.Windows.Forms.MenuStrip();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ScoreNumber = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Счёт = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.MenuItemExit.SuspendLayout();
+            this.MenuItemBack.SuspendLayout();
+            this.MenuItemStart.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.Счёт.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuItemExit
+            // 
+            this.MenuItemExit.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuItemExit.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.MenuItemExit.Location = new System.Drawing.Point(0, 60);
+            this.MenuItemExit.Name = "MenuItemExit";
+            this.MenuItemExit.Size = new System.Drawing.Size(582, 30);
+            this.MenuItemExit.TabIndex = 0;
+            this.MenuItemExit.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(67, 26);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // MenuItemBack
+            // 
+            this.MenuItemBack.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuItemBack.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.назадToolStripMenuItem});
+            this.MenuItemBack.Location = new System.Drawing.Point(0, 30);
+            this.MenuItemBack.Name = "MenuItemBack";
+            this.MenuItemBack.Size = new System.Drawing.Size(582, 30);
+            this.MenuItemBack.TabIndex = 1;
+            this.MenuItemBack.Text = "menuStrip2";
+            // 
+            // назадToolStripMenuItem
+            // 
+            this.назадToolStripMenuItem.Name = "назадToolStripMenuItem";
+            this.назадToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
+            this.назадToolStripMenuItem.Text = "Назад";
+            // 
+            // MenuItemStart
+            // 
+            this.MenuItemStart.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuItemStart.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem});
+            this.MenuItemStart.Location = new System.Drawing.Point(0, 0);
+            this.MenuItemStart.Name = "MenuItemStart";
+            this.MenuItemStart.Size = new System.Drawing.Size(582, 30);
+            this.MenuItemStart.TabIndex = 2;
+            this.MenuItemStart.Text = "menuStrip1";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(103, 26);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ScoreNumber});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 567);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(582, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ScoreNumber
+            // 
+            this.ScoreNumber.Name = "ScoreNumber";
+            this.ScoreNumber.Size = new System.Drawing.Size(17, 20);
+            this.ScoreNumber.Text = "0";
+            // 
+            // Счёт
+            // 
+            this.Счёт.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Счёт.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.Счёт.Location = new System.Drawing.Point(0, 541);
+            this.Счёт.Name = "Счёт";
+            this.Счёт.Size = new System.Drawing.Size(582, 26);
+            this.Счёт.TabIndex = 4;
+            this.Счёт.Text = "statusStrip2";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(43, 20);
+            this.toolStripStatusLabel1.Text = "Счёт:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(444, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 57);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(300, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 57);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(156, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 57);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 57);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(444, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 57);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(300, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 57);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(156, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 57);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(12, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 57);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(444, 329);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 57);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "label9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(300, 329);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 57);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "label10";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(156, 329);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(130, 57);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "label11";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(12, 329);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(130, 57);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "label12";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(444, 468);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(130, 57);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "label13";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(300, 468);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(130, 57);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "label14";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(156, 468);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(130, 57);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "label15";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(12, 468);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(130, 57);
+            this.label16.TabIndex = 20;
+            this.label16.Text = "label16";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(582, 593);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Счёт);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.MenuItemExit);
+            this.Controls.Add(this.MenuItemBack);
+            this.Controls.Add(this.MenuItemStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.MenuItemExit;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MenuItemExit.ResumeLayout(false);
+            this.MenuItemExit.PerformLayout();
+            this.MenuItemBack.ResumeLayout(false);
+            this.MenuItemBack.PerformLayout();
+            this.MenuItemStart.ResumeLayout(false);
+            this.MenuItemStart.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.Счёт.ResumeLayout(false);
+            this.Счёт.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuItemExit;
+        private System.Windows.Forms.MenuStrip MenuItemBack;
+        private System.Windows.Forms.MenuStrip MenuItemStart;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem назадToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel ScoreNumber;
+        private System.Windows.Forms.StatusStrip Счёт;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
