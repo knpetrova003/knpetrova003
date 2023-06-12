ad85511b7aa5e1e6634254126d9d673c925859bb
Kseniya <knpetrova00@icloud.com>
2023-06-12 17:00:41 +0500

    задание4

---
+++ knpetrova003--4.1/knpetrova003--4.1/knpetrova003--4.1/knpetrova003--4.1/knpetrova003--3/knpetrova003--3/UGI-216004-main/PhotoEnhancer/Filters/Transform/ReplaceOddRowsFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class ReplaceOddRowsFilter : ParametrizedFilter<EmptyParameters>
+    {
+        ReplaceOddRowsTransformer transformer;
+
+        public ReplaceOddRowsFilter(string name,
+            ReplaceOddRowsTransformer transformer)
+        {
+            this.name = name;
+            this.transformer = transformer;
+        }
+
+        public override Photo Process(Photo original, EmptyParameters parameters)
+        {
+            var oldSize = new Size(original.Width, original.Height);
+            transformer.Initialize(oldSize, parameters);
+
+            var result = new Photo(transformer.ResultSize.Width, transformer.ResultSize.Height);
+
+            for (var x = 0; x < result.Width; x++)
+                for (var y = 0; y < result.Height; y++)
+                {
+                    var oldPoint = transformer.MapPoint(new Point(x, y));
+                    if (oldPoint.HasValue)
+                        result[x, y] = original[oldPoint.Value.X, oldPoint.Value.Y];
+                }
+
+            return result;
+        }
+    }
+}
---
+++ knpetrova003--4.1/knpetrova003--4.1/knpetrova003--4.1/knpetrova003--4.1/knpetrova003--3/knpetrova003--3/UGI-216004-main/PhotoEnhancer/Filters/Transform/ShiftTransformer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class MoveTransformer : ITransformer<ShiftParameters>
+    {
+        double movePercent { get; set; }
+
+        public Size ResultSize { get; private set; }
+
+        public void Initialize(Size size, ShiftParameters parameters)
+        {
+            movePercent = parameters.MoveInPercent / 100;
+            ResultSize = size;
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            return new Point((point.X - (int)(movePercent * ResultSize.Width) + ResultSize.Width) % ResultSize.Width,
+                point.Y);
+        }
+    }
+}
