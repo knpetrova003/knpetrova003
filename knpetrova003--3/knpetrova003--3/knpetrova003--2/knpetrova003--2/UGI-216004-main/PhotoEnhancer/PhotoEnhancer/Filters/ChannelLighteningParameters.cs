11ae486e85234e07e710dc8377ee55a9d60c8c72
Kseniya <knpetrova00@icloud.com>
2023-06-12 17:49:41 +0500

    задание4

---
+++ knpetrova003--3/knpetrova003--3/knpetrova003--2/knpetrova003--2/UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/ChannelLighteningParameters.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    internal class ChannelsParameters
+    {
+        public class ChannelLighteningParameters : IParameters
+        {
+            public double Rk { get; set; } // Параметр для канала R
+            public double Gk { get; set; } // Параметр для канала G
+            public double Bk { get; set; } // Параметр для канала B
+
+            public ParameterInfo[] GetDecription()
+            {
+                return new[]
+                {
+                new ParameterInfo()
+                {
+                    Name = "Коэффициент R",
+                    MinValue = 0,
+                    MaxValue = 10,
+                    DefaultValue = 1,
+                    Increment = 0.05
+                },
+                new ParameterInfo()
+                {
+                    Name = "Коэффициент G",
+                    MinValue = 0,
+                    MaxValue = 10,
+                    DefaultValue = 1,
+                    Increment = 0.05
+                },
+                new ParameterInfo()
+                {
+                    Name = "Коэффициент B",
+                    MinValue = 0,
+                    MaxValue = 10,
+                    DefaultValue = 1,
+                    Increment = 0.05
+                }
+            };
+            }
+
+            public void SetValues(double[] values)
+            {
+                Rk = values[0];
+                Gk = values[1];
+                Bk = values[2];
+            }
+        }
+    }
+
+}
