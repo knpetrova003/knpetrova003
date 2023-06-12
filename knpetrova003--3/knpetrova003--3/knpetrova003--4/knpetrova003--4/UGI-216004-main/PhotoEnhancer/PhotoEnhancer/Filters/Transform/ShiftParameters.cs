8b580ecde56712ce6ff76c29513e20aa3677c1a7
Kseniya <knpetrova00@icloud.com>
2023-06-12 17:56:12 +0500

    задание4

---
+++ knpetrova003--3/knpetrova003--3/knpetrova003--4/knpetrova003--4/UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/Transform/ShiftParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class ShiftParameters : IParameters
+    {
+        public double MoveInPercent { get; set; }
+        public ParameterInfo[] GetDecription()
+        {
+            return new[]
+            {
+                new ParameterInfo()
+                {
+
+
+                    MinValue = 0,
+                    MaxValue = 100,
+
+                    DefaultValue = 0,
+                    Increment = 5,
+
+                    Name = "Сдвиг вправо в процентах",
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            MoveInPercent = values[0];
+        }
+    }
+}
