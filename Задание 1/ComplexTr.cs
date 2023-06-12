8c365ef3d8bd34f319dac75e315c7578d27042c0
Kseniya <knpetrova00@icloud.com>
2023-06-12 13:59:28 +0500

    задание4

---
+++ Задание 1/ComplexTr.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Homework_1
+{
+    public struct ComplexTr
+    {
+        private float _abs;
+        public float Abs
+        {
+            get => _abs;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Модуль должен быть неотрицательным");
+                _abs = value;
+            }
+        }
+
+        private float _arg;
+        public float Arg
+        {
+            get => _arg;
+            set => _arg = value;
+        }
+
+        public float Re => _abs * (float)Math.Cos(_arg);
+        public float Im => _abs * (float)Math.Sin(_arg);
+
+        public ComplexTr(float abs, float arg)
+        {
+            if (abs < 0)
+                throw new ArgumentException("Модуль должен быть неотрицательным");
+            _abs = abs;
+            _arg = arg;
+        }
+
+        public override string ToString()
+        {
+            if (FloatEquals(_abs, 0))
+                return "0";
+            if (FloatEquals(_abs, 1))
+                return $"cos({_arg}) + i sin({_arg})";
+            return $"{_abs}(cos({_arg}) + i sin({_arg}))";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not ComplexTr)
+                throw new ArgumentException("Сравнивать можно только комплексные числа");
+            var complex = (ComplexTr)obj;
+            return FloatEquals(Abs, complex.Abs)
+                && FloatEquals(0, (float)((Arg - complex.Arg) % (2 * Math.PI)));
+        }
+
+        private static bool FloatEquals(float a, float b)
+        {
+            var difference = Math.Pow(10, -13);
+            return Math.Abs(a - b) <= difference;
+        }
+        
+        public static ComplexTr operator *(ComplexTr a, ComplexTr b)
+        {
+            return new ComplexTr(a.Abs * b.Abs, a.Arg + b.Arg);
+        }
+        
+        public static ComplexTr operator /(ComplexTr a, ComplexTr b)
+        {
+            if (FloatEquals(b.Abs, 0))
+                throw new ArgumentException("Деление на 0 запрещено");
+            return new ComplexTr(a.Abs / b.Abs, a.Arg - b.Arg);
+        }
+    }
+}
---
+++ Задание 1/Tests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+namespace Homework_1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(0,0)]
+        [TestCase(1.2f,42)]
+        [TestCase(5,-10)]
+        public void ConstructorTest(float abs, float arg)
+        {
+            var complex = new ComplexTr(abs, arg);
+            Assert.That(complex.Abs, Is.EqualTo(abs));
+            Assert.That(complex.Arg, Is.EqualTo(arg));
+        }
+        
+        [Test]
+        public void NegativeAbsConstructorTest()
+        {
+            Assert.That(() => new ComplexTr(-1, 0), Throws.ArgumentException);
+        }
+        
+        [Test]
+        public void NegativeAbsTest()
+        {
+            var complex = new ComplexTr();
+            Assert.That(() => complex.Abs = -1, Throws.ArgumentException);
+        }
+        
+        [TestCase(0,15, "0")]
+        [TestCase(0,0, "0")]
+        [TestCase(1,50.5f, "cos(50,5) + i sin(50,5)")]
+        [TestCase(2.33f,-50.5f, "2,33(cos(-50,5) + i sin(-50,5))")]
+        public void ToStringTest(float abs, float arg, string expected)
+        {
+            var complex = new ComplexTr(abs, arg);
+            Assert.That(complex.ToString(), Is.EqualTo(expected));
+        }
+
+        [TestCase(0, 0, 1, 5)]
+        [TestCase(5, 0, 1.2f, -5)]
+        public void MultiplicationTest(float absA, float argA, float absB, float argB)
+        {
+            var a = new ComplexTr(absA, argA);
+            var b = new ComplexTr(absB, argB);
+            var expectedAbs = absA * absB;
+            var expectedArg = argA + argB;
+            var actualComplex = a * b;
+            Assert.That(actualComplex.Abs, Is.EqualTo(expectedAbs));
+            Assert.That(actualComplex.Arg, Is.EqualTo(expectedArg));
+        }
+        
+        [TestCase(0, 0, 1, 5)]
+        [TestCase(5, 0, 1.2f, -5)]
+        public void DivisionTest(float absA, float argA, float absB, float argB)
+        {
+            var a = new ComplexTr(absA, argA);
+            var b = new ComplexTr(absB, argB);
+            var expectedAbs = absA / absB;
+            var expectedArg = argA - argB;
+            var actualComplex = a / b;
+            Assert.That(actualComplex.Abs, Is.EqualTo(expectedAbs));
+            Assert.That(actualComplex.Arg, Is.EqualTo(expectedArg));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1.2f, 4)]
+        public void DivisionZeroTest(float abs, float arg)
+        {
+            var complex = new ComplexTr(abs, arg);
+            Assert.That(() => complex / new ComplexTr(0, 1), Throws.ArgumentException);
+        }
+    }
+}
