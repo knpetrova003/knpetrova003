6c6786c237db3b2c69b68d7ac1f52681df0de45c
Kseniya <knpetrova00@icloud.com>
2023-06-15 20:58:37 +0500

    5

---
+++ Homework 5 — копия/Homework 5/Record.cs
@@ -0,0 +1,7 @@
+﻿public struct Record
+{
+    public int ClientID; // идентификационный номер клиента
+    public int Year;     // год
+    public int Month;    // номер месяца
+    public int Duration; // продолжительность занятий в данном месяце данного года (в часах)
+}
---
+++ Homework 5 — копия/Homework 5/RecordsTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RecordsTask
+{
+    // Метод для вывода информации о годах и месяцах с суммарной длительностью занятий более 10% от года
+    public void PrintNumberOfMonthsPerYear(List<Record> db)
+    {
+        db
+            .GroupBy(r => r.Year) // группируем записи по годам
+            .Select(g => new // преобразуем каждую группу в объект
+            {
+                Year = g.Key, // год
+                Months = g.GroupBy(r => r.Month) // подгруппируем записи по месяцам
+                    .Count(mg => mg.Sum(r => r.Duration) > 0.1 * g.Sum(r => r.Duration)) // подсчитаем количество месяцев с суммарной длительностью более 10% от суммарной длительности за год
+            })
+            .Where(x => x.Months > 0) // фильтруем года, в которых есть такие месяцы
+            .OrderByDescending(x => x.Months) // сортируем по убыванию количества месяцев
+            .ThenBy(x => x.Year) // затем по возрастанию номера года
+            .ToList() // преобразуем в список
+            .ForEach(x => Console.WriteLine($"{x.Year} {x.Months}")); // выводим на консоль
+    }
+}
